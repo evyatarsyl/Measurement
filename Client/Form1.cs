97ce310d04b0178feb48e3131c9ac1ec0ce22329
Evyatar Sylvetsky <Evyatar Sylvetsky>
2015-11-21 19:51:45 +0200

    Adding basic project files

---
+++ Client/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        static int Measured = 0;
+        const int interval = 1000;
+
+        static string path = @"C:\Users\Evyatar\Desktop\Measure.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                StreamWriter sw = File.CreateText(path);
+                sw.Write(4);
+                sw.Close();
+            }
+            StreamReader sr = new StreamReader(path);
+            int border = Convert.ToInt32(sr.ReadLine());
+            sr.Close();
+
+            //------------------------------------------//
+
+            while (true)
+            {
+                Measured += 1;
+                if (Measured >= border)
+                {
+                    TcpClient client = new TcpClient("localhost", 2444);
+                    try
+                    {
+                        Stream s = client.GetStream();
+                        StreamReader NetSR = new StreamReader(s);
+                        StreamWriter NetSW = new StreamWriter(s);
+                        NetSW.AutoFlush = true;
+                        NetSW.WriteLine("done");
+                        string cmd = NetSR.ReadLine();
+                        if (cmd == "off")
+                        {
+                            NetSR.Close();
+                            NetSW.Close();
+                            s.Close();
+                            client.Close();
+                            System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                            proc.StartInfo.FileName = @"C:\Users\Evyatar\Desktop\batchfile.bat";
+                            proc.StartInfo.RedirectStandardError = false;
+                            proc.StartInfo.RedirectStandardOutput = false;
+                            proc.StartInfo.UseShellExecute = true;
+                            proc.Start();
+                            proc.WaitForExit();
+                        }
+                        else
+                        {
+                            MessageBox.Show("אתה יכול להיות עוד על המחשב");
+                        }
+                    }
+
+
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: /n/n" + ex);
+                        client.Close();
+                    }
+
+
+                }
+                Thread.Sleep(interval);
+            }
+        }
+    }
+}
---
+++ Server/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Thread listen = new Thread(new ThreadStart(Service));
+        static TcpListener listener;
+        static string cmd;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listen.Start();
+        }
+        
+        static void Service()
+        {
+            listener = new TcpListener(2444);
+            listener.Start();
+            Socket soc = listener.AcceptSocket();
+            NetworkStream NetStream = new NetworkStream(soc);
+            StreamReader NetSR = new StreamReader(NetStream);
+            StreamWriter NetSW = new StreamWriter(NetStream);
+            NetSW.AutoFlush = true;
+            string cmd = NetSR.ReadLine();
+            Console.WriteLine(cmd);
+            if (cmd == "done")
+            {
+                string caption = "Timeout";
+                string message = "זמן המחשב שהקצבת לילדך תם. האם ברצונך לכבות את מחשבו?";
+                MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                DialogResult result;
+                result = MessageBox.Show(message, caption, buttons);
+                if (result == DialogResult.Yes)
+                {
+                    NetSW.WriteLine("off");
+                    NetSW.Close();
+                    NetSR.Close();
+                }
+                else
+                {
+                    NetSW.WriteLine("on");
+                }
+                
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            listen.Abort();
+            listen.Join();
+        }
+    }
+}
